50e810def38e7fd15441e72bde34904d4d40172a
Dave Farnham <dave.farnham@icloud.com>
2023-05-07 18:35:18 -0600

    use clap 4

--- uuids/src/main.rs
+++ uuids/src/main.rs
@@ -15,12 +15,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     #[clap(author, version, about, long_about=None)]
     struct Args {
         /// Version 4, output a random v4 uuid
-        #[clap(short = '4')]
+        #[clap(short = '4', group = "algorithm")]
         v4: bool,
 
         /// Version 5, namespace OID on the input -- this is the default
         #[allow(dead_code)]
-        #[clap(short = '5')]
+        #[clap(short = '5', group = "algorithm")]
         v5: bool,
 
         /// Quiet mode, output only the UUID, suppress filename
@@ -28,8 +28,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         quiet: bool,
 
         /// file|stdin, filename of "-" implies stdin
-        #[clap(parse(from_os_str), multiple_values = false)]
-        file: Option<std::path::PathBuf>,
+        files: Vec<std::path::PathBuf>,
     }
     let args = Args::parse();
 
@@ -44,29 +43,36 @@ fn main() -> Result<(), Box<dyn Error>> {
         return Ok(());
     }
 
-    // allocate a buffer to receive data from stdin|file, note a filename of "-" implies stdin
-    let mut buffer = vec![];
-    let input_name: String = match args.file {
-        Some(file) if file.as_os_str() != "-" => {
-            File::open(&file)
-                .with_context(|| format!("could not open file `{:?}`", file.as_os_str()))?
-                .read_to_end(&mut buffer)
-                .with_context(|| format!("could not read file `{:?}`", file.as_os_str()))?;
-            file.to_string_lossy().into()
-        }
-        _ => {
-            io::stdin()
-                .read_to_end(&mut buffer)
-                .with_context(|| "could not read `stdin`")?;
-            "<stdin>".into()
-        }
+    let files = match args.files.is_empty() {
+        true => vec![std::path::PathBuf::from("-")],
+        false => args.files,
     };
 
-    // compute a version 5 uuid using namespace OID on the input
-    let uuid5 = Uuid::new_v5(&Uuid::NAMESPACE_OID, &buffer);
-    match args.quiet {
-        true => writeln!(stdout, "{uuid5}")?,
-        false => writeln!(stdout, "uuid5 ({input_name}) = {uuid5}")?,
+    for file in files {
+        // allocate a buffer to receive data from stdin|file, note a filename of "-" implies stdin
+        let mut buffer = vec![];
+        let input_name: String = match file.as_os_str() != "-" {
+            true => {
+                File::open(&file)
+                    .with_context(|| format!("could not open file `{:?}`", file.as_os_str()))?
+                    .read_to_end(&mut buffer)
+                    .with_context(|| format!("could not read file `{:?}`", file.as_os_str()))?;
+                file.to_string_lossy().into()
+            }
+            false => {
+                io::stdin()
+                    .read_to_end(&mut buffer)
+                    .with_context(|| "could not read `stdin`")?;
+                "<stdin>".into()
+            }
+        };
+
+        // compute a version 5 uuid using namespace OID on the input
+        let uuid5 = Uuid::new_v5(&Uuid::NAMESPACE_OID, &buffer);
+        match args.quiet {
+            true => writeln!(stdout, "{uuid5}")?,
+            false => writeln!(stdout, "uuid5 ({input_name}) = {uuid5}")?,
+        }
     }
 
     Ok(())
