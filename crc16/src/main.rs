329397332c3df08eacda7cc014533027766aeb45
Dave Farnham <dave.farnham@icloud.com>
2023-07-13 20:11:34 -0600

    playing with const functions to generate the table

--- crc16/src/main.rs
+++ crc16/src/main.rs
@@ -4,10 +4,6 @@ use std::error::Error;
 use std::fs::File;
 use std::io::{self, Read};
 
-#[rustfmt::skip]
-const MSB: u32     = 0x80000000; // Most significant bit
-const CRCPOLY: u32 = 0x80050000; // x^16 + x^15 + x^2 + 1 (shifted << 16)
-
 // CRC-16, CRC-16/UMTS
 //
 // https://reveng.sourceforge.io/crc-catalogue/all.htm
@@ -19,8 +15,73 @@ const CRCPOLY: u32 = 0x80050000; // x^16 + x^15 + x^2 + 1 (shifted << 16)
 // check=0xfee8
 // residue=0x0000
 // name="CRC-16/UMTS"
-fn crc16(msg: &[u8]) -> u16 {
-    let mut crc;
+
+#[rustfmt::skip]
+#[allow(dead_code)]
+const MSB: u32     = 0x80000000; // Most significant bit
+
+#[rustfmt::skip]
+#[allow(dead_code)]
+const CRCPOLY: u32 = 0x80050000; // x^16 + x^15 + x^2 + 1 (shifted << 16)
+
+#[rustfmt::skip]
+#[allow(dead_code)]
+// Pre-generated table
+const TABLE: [u16; 256] = [
+        0, 32773, 32783,    10, 32795,    30,    20, 32785, 32819,    54,    60, 32825,    40, 32813, 32807,    34,
+    32867,   102,   108, 32873,   120, 32893, 32887,   114,    80, 32853, 32863,    90, 32843,    78,    68, 32833,
+    32963,   198,   204, 32969,   216, 32989, 32983,   210,   240, 33013, 33023,   250, 33003,   238,   228, 32993,
+      160, 32933, 32943,   170, 32955,   190,   180, 32945, 32915,   150,   156, 32921,   136, 32909, 32903,   130,
+    33155,   390,   396, 33161,   408, 33181, 33175,   402,   432, 33205, 33215,   442, 33195,   430,   420, 33185,
+      480, 33253, 33263,   490, 33275,   510,   500, 33265, 33235,   470,   476, 33241,   456, 33229, 33223,   450,
+      320, 33093, 33103,   330, 33115,   350,   340, 33105, 33139,   374,   380, 33145,   360, 33133, 33127,   354,
+    33059,   294,   300, 33065,   312, 33085, 33079,   306,   272, 33045, 33055,   282, 33035,   270,   260, 33025,
+    33539,   774,   780, 33545,   792, 33565, 33559,   786,   816, 33589, 33599,   826, 33579,   814,   804, 33569,
+      864, 33637, 33647,   874, 33659,   894,   884, 33649, 33619,   854,   860, 33625,   840, 33613, 33607,   834,
+      960, 33733, 33743,   970, 33755,   990,   980, 33745, 33779,  1014,  1020, 33785,  1000, 33773, 33767,   994,
+    33699,   934,   940, 33705,   952, 33725, 33719,   946,   912, 33685, 33695,   922, 33675,   910,   900, 33665,
+      640, 33413, 33423,   650, 33435,   670,   660, 33425, 33459,   694,   700, 33465,   680, 33453, 33447,   674,
+    33507,   742,   748, 33513,   760, 33533, 33527,   754,   720, 33493, 33503,   730, 33483,   718,   708, 33473,
+    33347,   582,   588, 33353,   600, 33373, 33367,   594,   624, 33397, 33407,   634, 33387,   622,   612, 33377,
+      544, 33317, 33327,   554, 33339,   574,   564, 33329, 33299,   534,   540, 33305,   520, 33293, 33287,   514,
+];
+
+// TABLE generator
+//
+// Function to compute the TABLE using the standard algorithm
+#[allow(dead_code)]
+fn crc16_table_generator() -> Vec<u16> {
+    (0..256).map(|i| crc16_algo(&[0, i as u8])).collect()
+}
+
+// TABLE generator using 2 const functions
+//
+pub(crate) const fn _crc16_byte(byte: u8) -> u16 {
+    let mut value: u16 = (byte as u16) << 8;
+    let mut i = 0;
+    while i < 8 {
+        value = (value << 1) ^ (((value >> 15) & 1) * (CRCPOLY >> 16) as u16);
+        i += 1;
+    }
+    value
+}
+// Returns the CRC-16 TABLE
+//
+// assert_eq!(TABLE, crc16_table());
+pub(crate) const fn crc16_table() -> [u16; 256] {
+    let mut table = [0u16; 256];
+    let mut i = 0;
+    while i < table.len() {
+        table[i] = _crc16_byte(i as u8);
+        i += 1;
+    }
+    table
+}
+
+// The first algorithm implementation I learned
+#[allow(dead_code)]
+fn crc16_algo(msg: &[u8]) -> u16 {
+    let mut crc: u32;
 
     // Minimum input is 2 bytes
     assert!(msg.len() > 1, "Invalid input");
@@ -41,6 +102,24 @@ fn crc16(msg: &[u8]) -> u16 {
         }
     }
     ((crc >> 16) & 0x0000ffff) as u16
+}
+
+// The algorithm using a table lookup
+fn crc16(msg: &[u8]) -> u16 {
+    // Message must have length > 0
+    assert!(!msg.is_empty(), "Invalid input");
+
+    let mut crc = 0u16;
+
+    // Get the table from a const function
+    let table = crc16_table();
+
+    for byte in msg.iter() {
+        crc ^= (*byte as u16) << 8;
+        //crc = (crc << 8) ^ TABLE[(crc >> 8) as usize];
+        crc = (crc << 8) ^ table[(crc >> 8) as usize];
+    }
+    crc
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -78,6 +157,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         // Output CRC-16/UMTS
         println!("{input_name}: {}", crc16(&msg));
+        //println!("table = {:?}", crc16_table());
     }
 
     Ok(())
@@ -122,26 +202,44 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_bad_input_1() {
-        crc16(&[]);
+        crc16_algo(&[]);
     }
 
     #[test]
     #[should_panic]
     fn test_bad_input_2() {
-        crc16(&[1]);
+        crc16_algo(&[1]);
     }
 
     #[test]
     fn test_zero() {
         let data = [0, 0];
         assert_eq!(0, crc16(&data));
+        assert_eq!(crc16(&data), crc16_algo(&data));
         assert_eq!(crc16(&data), CRC_16_UMTS.checksum(&data));
+    }
+
+    #[test]
+    fn table_test() {
+        let table = crc16_table();
+        assert_eq!(TABLE, table);
+
+        for (i, val) in crc16_table_generator().into_iter().enumerate() {
+            assert_eq!(TABLE[i], val);
+            assert_eq!(table[i], val);
+        }
+
+        for i in 0..256 {
+            assert_eq!(TABLE[i], crc16(&[i as u8]));
+            assert_eq!(TABLE[i], crc16_algo(&[0, i as u8]));
+        }
     }
 
     #[test]
     fn test1() {
         let data = [5, 0, 255, 255, 255, 255, 0, 0, 0, 0, 2, 0, 1, 1, 0, 0];
         assert_eq!(35273, crc16(&data));
+        assert_eq!(crc16(&data), crc16_algo(&data));
         assert_eq!(crc16(&data), CRC_16_UMTS.checksum(&data));
     }
 
@@ -151,6 +249,7 @@ mod tests {
             170, 170, 170, 170, 170, 170, 170, 170, 204, 204, 204, 204, 204, 204, 204, 204,
         ];
         assert_eq!(43036, crc16(&data));
+        assert_eq!(crc16(&data), crc16_algo(&data));
         assert_eq!(crc16(&data), CRC_16_UMTS.checksum(&data));
     }
 
@@ -158,9 +257,11 @@ mod tests {
     fn test3() {
         let data = [100, 97, 118, 101];
         assert_eq!(25309, crc16(&data));
+        assert_eq!(crc16(&data), crc16_algo(&data));
         assert_eq!(crc16(&data), CRC_16_UMTS.checksum(&data));
 
         assert_eq!(25309, crc16(b"dave"));
+        assert_eq!(crc16(b"dave"), crc16_algo(b"dave"));
         assert_eq!(crc16(b"dave"), CRC_16_UMTS.checksum(b"dave"));
     }
 
@@ -168,9 +269,11 @@ mod tests {
     fn test4() {
         let data = [49, 50, 51, 52, 53, 54, 55, 56, 57];
         assert_eq!(65256, crc16(&data));
+        assert_eq!(crc16(&data), crc16_algo(&data));
         assert_eq!(crc16(&data), CRC_16_UMTS.checksum(&data));
 
         assert_eq!(65256, crc16(b"123456789"));
+        assert_eq!(crc16(b"123456789"), crc16_algo(b"123456789"));
         assert_eq!(crc16(b"123456789"), CRC_16_UMTS.checksum(b"123456789"));
     }
 
